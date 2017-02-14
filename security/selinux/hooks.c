afc553604751d11ecfd91679c1284c94a33be994
James Morris <jmorris@namei.org>
2017-02-15 00:18:51 +1100

    security: mark LSM hooks as __ro_after_init
    Mark all of the registration hooks as __ro_after_init (via the
    __lsm_ro_after_init macro).
    
    Signed-off-by: James Morris <james.l.morris@oracle.com>
    Acked-by: Stephen Smalley <sds@tycho.nsa.gov>
    Acked-by: Kees Cook <keescook@chromium.org>
    Change-Id: I3d76ce950824a535c651c8c1ca0e02e0783e12ce

--- security/selinux/hooks.c
+++ security/selinux/hooks.c
@@ -6168,7 +6168,7 @@ static void selinux_bpf_prog_free(struct bpf_prog_aux *aux)
 }
 #endif
 
-static struct security_hook_list selinux_hooks[] = {
+static struct security_hook_list selinux_hooks[] __lsm_ro_after_init = {
 	LSM_HOOK_INIT(binder_set_context_mgr, selinux_binder_set_context_mgr),
 	LSM_HOOK_INIT(binder_transaction, selinux_binder_transaction),
 	LSM_HOOK_INIT(binder_transfer_binder, selinux_binder_transfer_binder),
