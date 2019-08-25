afbf4b9714826b158d4effa26cae7d8ce5369f0e
derfelot <5124143+derfelot@users.noreply.github.com>
2019-08-25 23:38:01 +0200

    msm: kgsl: Don't use logging for adreno_regwrite as in Sony kernel
    Taken from Sony 47.2.A.10.107 stock kernel

--- drivers/gpu/msm/adreno.c
+++ drivers/gpu/msm/adreno.c
@@ -10,6 +10,11 @@
  * GNU General Public License for more details.
  *
  */
+/*
+ * NOTE: This file has been modified by Sony Mobile Communications Inc.
+ * Modifications are Copyright (c) 2018 Sony Mobile Communications Inc,
+ * and licensed under the license of the file.
+ */
 #include <linux/module.h>
 #include <linux/uaccess.h>
 #include <linux/sched.h>
@@ -2436,7 +2441,7 @@ static void adreno_regwrite(struct kgsl_device *device,
 	/*ensure previous writes post before this one,
 	 * i.e. act like normal writel() */
 	wmb();
-	__raw_writel(value, reg);
+	__raw_writel_no_log(value, reg);
 }
 
 /**
