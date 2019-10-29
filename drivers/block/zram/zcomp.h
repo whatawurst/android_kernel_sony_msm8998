c1a8b1eba761cc52443cf0d4a28e26745206fc80
derfelot <5124143+derfelot@users.noreply.github.com>
2019-10-29 20:46:00 +0100

    zram: Undo recent caf changes to be in line with Sony kernel
    Revert to pre-4.4.152 zram for Sony's z3fold implementation
    
    Taken from Sony 47.2.A.11.228 stock kernel

--- drivers/block/zram/zcomp.h
+++ drivers/block/zram/zcomp.h
@@ -13,15 +13,33 @@
 struct zcomp_strm {
 	/* compression/decompression buffer */
 	void *buffer;
-	struct crypto_comp *tfm;
+	/*
+	 * The private data of the compression stream, only compression
+	 * stream backend can touch this (e.g. compression algorithm
+	 * working memory)
+	 */
+	void *private;
+};
+
+/* static compression backend */
+struct zcomp_backend {
+	int (*compress)(const unsigned char *src, unsigned char *dst,
+			size_t *dst_len, void *private);
+
+	int (*decompress)(const unsigned char *src, size_t src_len,
+			unsigned char *dst);
+
+	void *(*create)(gfp_t flags);
+	void (*destroy)(void *private);
+
+	const char *name;
 };
 
 /* dynamic per-device compression frontend */
 struct zcomp {
 	struct zcomp_strm * __percpu *stream;
+	struct zcomp_backend *backend;
 	struct notifier_block notifier;
-
-	const char *name;
 };
 
 ssize_t zcomp_available_show(const char *comp, char *buf);
@@ -30,14 +48,14 @@ bool zcomp_available_algorithm(const char *comp);
 struct zcomp *zcomp_create(const char *comp);
 void zcomp_destroy(struct zcomp *comp);
 
-struct zcomp_strm *zcomp_stream_get(struct zcomp *comp);
-void zcomp_stream_put(struct zcomp *comp);
+struct zcomp_strm *zcomp_strm_find(struct zcomp *comp);
+void zcomp_strm_release(struct zcomp *comp, struct zcomp_strm *zstrm);
 
-int zcomp_compress(struct zcomp_strm *zstrm,
-		const void *src, unsigned int *dst_len);
+int zcomp_compress(struct zcomp *comp, struct zcomp_strm *zstrm,
+		const unsigned char *src, size_t *dst_len);
 
-int zcomp_decompress(struct zcomp_strm *zstrm,
-		const void *src, unsigned int src_len, void *dst);
+int zcomp_decompress(struct zcomp *comp, const unsigned char *src,
+		size_t src_len, unsigned char *dst);
 
 bool zcomp_set_max_streams(struct zcomp *comp, int num_strm);
 #endif /* _ZCOMP_H_ */
