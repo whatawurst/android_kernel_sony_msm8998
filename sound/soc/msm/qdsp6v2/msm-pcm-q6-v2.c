63efce97fa2189823cc1dd8fcc7f73239b988c21
Soumya Managoli <quic_c_smanag@quicinc.com>
2023-01-06 14:37:20 +0530

    ASoC: msm-pcm-q6-v2: Add dsp buf check
    Current logic copies user buf size of data
    from the avail dsp buf at a given offset.
    If this offset returned from DSP in READ_DONE event
    goes out of bounds or is corrupted, then it can lead to
    out of bounds DSP buffer access, resulting in memory fault.
    Fix is to add check for this buf offset, if it is within
    the buf size range.
    
    Change-Id: I7753cc6db394704dbb959477150141d42b836bef
    Signed-off-by: Soumya Managoli <quic_c_smanag@quicinc.com>

--- sound/soc/msm/qdsp6v2/msm-pcm-q6-v2.c
+++ sound/soc/msm/qdsp6v2/msm-pcm-q6-v2.c
@@ -1,13 +1,6 @@
+// SPDX-License-Identifier: GPL-2.0-only
 /* Copyright (c) 2012-2021, The Linux Foundation. All rights reserved.
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License version 2 and
- * only version 2 as published by the Free Software Foundation.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * Copyright (c) 2023 Qualcomm Innovation Center, Inc. All rights reserved.
  */
 
 
@@ -990,6 +983,14 @@ static int msm_pcm_capture_copy(struct snd_pcm_substream *substream,
 			xfer = size;
 		offset = prtd->in_frame_info[idx].offset;
 		pr_debug("Offset value = %d\n", offset);
+
+		if (offset >= size) {
+			pr_err("%s: Invalid dsp buf offset\n", __func__);
+			ret = -EFAULT;
+			q6asm_cpu_buf_release(OUT, prtd->audio_client);
+			goto fail;
+		}
+
 		if (size == 0 || size < prtd->pcm_count) {
 			memset(bufptr + offset + size, 0, prtd->pcm_count - size);
 			if (fbytes > prtd->pcm_count)
